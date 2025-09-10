b586c1a94d83a42463c9737dca3259e444e0e7a6
Maanibhadra Digital -bharuch <sgsbackup1@gmail.com>
2025-09-10 17:53:50 +0530

    Boilerplat Vite,Tailwind,PWA plugin added

--- src/App.jsx
+++ src/App.jsx
@@ -5,25 +5,23 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+   
+        <div className="flex items-center justify-center min-h-screen bg-gray-100">
+          <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
+            <div className="shrink-0">
+              <svg className="h-12 w-12 text-blue-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
+              </svg>
+            </div>
+            <div>
+              <div className="text-xl font-medium text-black">Tailwind CSS Test</div>
+              <p className="text-gray-500">If you see a blue circle with a checkmark, Tailwind is working! 🎉</p>
+            </div>
+          </div>
+        </div>
+      );
+    };
     
-    
-      <h1 class="text-xl font-bold underline">Vite + React</h1>
-      <button class="bg-sky-500 hover:bg-sky-700 ...">Save changes</button>
-
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+  
 
 export default App
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,33 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(),tailwindcss()],
-})
+import { VitePWA } from 'vite-plugin-pwa'
 
+export default defineConfig({
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.svg', 'robots.txt', 'apple-touch-icon.png'],
+      manifest: {
+        name: 'Visha Oshwal BVPV',
+        short_name: 'VO Dir',
+        description: 'Visha Oshwal Dir (Borsad,Valvod,Padara,Vatadara)',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ]
+})
